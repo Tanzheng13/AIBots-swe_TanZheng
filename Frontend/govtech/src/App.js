8332d524c41cd363698abbdd88b4cc59d9e00874
Tanzheng13 <tanzheng1312@gmail.com>
2024-02-18 15:11:18 +0800

    submit function

--- Frontend/govtech/src/App.js
+++ Frontend/govtech/src/App.js
@@ -1,7 +1,14 @@
 import './App.css';
 import "./normal.css"
 
+
 function App() {
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    console.log("submit")
+  }
+
   return (
     <div className="App">
     <aside className="sidemenu">
@@ -15,19 +22,31 @@ function App() {
     <section className="chatbox">
       <div className="chat-log">
         <div className="chat-message">
-        <div className="chat-message-center">
-          <div className="avatar">
+          <div className="chat-message-center">
+            <div className="avatar">
             
             </div>
             <div className="message">
               Hello
             </div>
+          </div>
         </div>
+        <div className="chat-message chatgpt">
+          <div className="chat-message-center">
+            <div className="avatar chatgpt">
+            
+            </div>
+            <div className="message">
+              AI
+            </div>
+          </div>
         </div>
       </div>
     <div className="chat-input-holder"> 
-      <textarea rows="1" className="chat-input-area" placeholder="Type your message here">
-      </textarea>
+      <form onSubmit={handleSubmit}>
+        <input rows="1" className="chat-input-area" placeholder="Type your message here">
+        </input>
+      </form>
     </div>
     </section>
     </div>
